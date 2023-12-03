c3c1fea8f5683700d263aa2e2c68538a95366119
Brice Vandeputte <brice.vandeputte@orange.com>
2023-12-03 16:06:54 +0100

    merapi++

--- front-end/src/core/Live.js
+++ front-end/src/core/Live.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
 import ApiV0 from "../service/ApiV0";
+function isSet(variable) {
+    return (variable !== undefined && variable !== null);
+}
 
 export default class Live extends Component {
 
-    getEmbedUrlFrom(liveUrl) {// credit - Sobral, J W, zurfyx : https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
+    getEmbedUrlFrom(live) {
+        const liveUrl = live.url
+        const youtubeEmbed = this.getYoutubeEmbedUrl(liveUrl);
+        if (youtubeEmbed !== null) {
+            return youtubeEmbed;
+        }
+        const embedUrl = this.getEmbedUrl(live.embedUrl)
+        if (isSet(embedUrl)) {
+            return embedUrl;
+        }
+        return null;
+    }
+
+    getYoutubeEmbedUrl(url) {// credit - Sobral, J W, zurfyx : https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
         let rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|&v(?:i)?=))([^#&?]+).*/;
-        let r = liveUrl.match(rx);
+        let r = url.match(rx);
         return r && r.length > 0 ? "https://www.youtube.com/embed/" + r[1] + "?enablejsapi=1" : null;
+    }
+
+    getEmbedUrl(url) {
+        if (!isSet(url)) {
+            return null
+        }
+        let timestamp = new Date().getTime();
+        let timestampSec = Math.round(timestamp/1000);
+        return url.replaceAll("TIMESTAMPSEC", timestampSec);
     }
 
     render() {
@@ -14,13 +39,13 @@ export default class Live extends Component {
         if (!ApiV0.isSet(live)) {
             return null;
         }
-        const embedUrl = this.getEmbedUrlFrom(live.url);
+        const embedUrl = this.getEmbedUrlFrom(live);
         const typeImage = (live.type === 'earthquakes') ? "/earthquakes.jpg"
             : "/volcano_view.jpg";
         return (<div className="livesEntry">
             <a href={live.url} target="_live">
                 {embedUrl ? (
-                    <iframe width="300" height="215" src={embedUrl} title="YouTube video player"
+                    <iframe width="300" height="250" src={embedUrl} title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowFullScreen></iframe>
                 ) : (<>
